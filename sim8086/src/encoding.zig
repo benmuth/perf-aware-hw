e7b6903d035e6fdaee019221217f04c79ef046a5
Ben Muthalaly <benmuthalaly@gmail.com>
2023-12-24 04:22:32 -0600

    Remove unnecessary switch statement

--- sim8086/src/encoding.zig
+++ sim8086/src/encoding.zig
@@ -7,11 +7,8 @@ pub fn decode(allocator: std.mem.Allocator, buf: []u8) !std.ArrayList(u8) {
     var i: usize = 0;
     while (i < buf.len) : (i += 2) { // NOTE: hardcoded to 2 byte instr
         var instruction_bytes = [2]u8{ buf[i], buf[i + 1] };
-        print("instruction bytes: {0b}\n", .{instruction_bytes});
         var instruction = try parseInstruction(instruction_bytes);
-        print("instruction: {any}\n", .{instruction});
         var line = try binToAsm(allocator, instruction);
-        print("line: {s}\n", .{line});
         try assembly.appendSlice(line);
     }
     return assembly;
@@ -19,9 +16,9 @@ pub fn decode(allocator: std.mem.Allocator, buf: []u8) !std.ArrayList(u8) {
 
 const ReadError = error{FailedToParse};
 
-// takes 2 bytes of binary data and returns an instruction with the six fields of the mov instruction filled out
-// NOTE: hardcoded to 2 byte instructions
+/// takes 2 bytes of binary data and returns an instruction with the six fields of the mov instruction filled out
 pub fn parseInstruction(bytes: [2]u8) ReadError!Instruction {
+    // NOTE: hardcoded to 2 byte instructions
     const b1 = bytes[0];
     const b2 = bytes[1];
     var parsed_instruction = Instruction{};
@@ -35,74 +32,17 @@ pub fn parseInstruction(bytes: [2]u8) ReadError!Instruction {
     return parsed_instruction;
 }
 
-// takes an instruction and returns a line of assembly
+/// takes an instruction and returns a line of assembly
 pub fn binToAsm(allocator: std.mem.Allocator, instruction: Instruction) ![]const u8 {
+    // flip src and dest register based on d bit
     var regs = [2]u8{ instruction.reg, instruction.rm };
     if (instruction.d == 0) {
         regs[0] = instruction.rm;
         regs[1] = instruction.reg;
     }
-    var regsAsm = [2][]const u8{ "", "" };
+    var regs_asm: [2][]const u8 = undefined;
     for (regs, 0..) |reg, i| {
-        switch (reg) {
-            0b000 => {
-                if (instruction.w == 0) {
-                    regsAsm[i] = "al";
-                } else {
-                    regsAsm[i] = "ax";
-                }
-            },
-            0b001 => {
-                if (instruction.w == 0) {
-                    regsAsm[i] = "cl";
-                } else {
-                    regsAsm[i] = "cx";
-                }
-            },
-            0b010 => {
-                if (instruction.w == 0) {
-                    regsAsm[i] = "dl";
-                } else {
-                    regsAsm[i] = "dx";
-                }
-            },
-            0b011 => {
-                if (instruction.w == 0) {
-                    regsAsm[i] = "bl";
-                } else {
-                    regsAsm[i] = "bx";
-                }
-            },
-            0b100 => {
-                if (instruction.w == 0) {
-                    regsAsm[i] = "ah";
-                } else {
-                    regsAsm[i] = "sp";
-                }
-            },
-            0b101 => {
-                if (instruction.w == 0) {
-                    regsAsm[i] = "ch";
-                } else {
-                    regsAsm[i] = "bp";
-                }
-            },
-            0b110 => {
-                if (instruction.w == 0) {
-                    regsAsm[i] = "dh";
-                } else {
-                    regsAsm[i] = "si";
-                }
-            },
-            0b111 => {
-                if (instruction.w == 0) {
-                    regsAsm[i] = "bh";
-                } else {
-                    regsAsm[i] = "di";
-                }
-            },
-            else => return ReadError.FailedToParse,
-        }
+        regs_asm[i] = reg_arr[reg][instruction.w];
     }
 
     var opcode: []const u8 = undefined;
@@ -110,12 +50,13 @@ pub fn binToAsm(allocator: std.mem.Allocator, instruction: Instruction) ![]const
         opcodes.MOV => opcode = "mov",
     }
 
+    // put parts together
     const asm_instruction = [_][]const u8{
         opcode,
         " ",
-        regsAsm[0],
+        regs_asm[0],
         ", ",
-        regsAsm[1],
+        regs_asm[1],
         "\n",
     };
 
@@ -123,8 +64,16 @@ pub fn binToAsm(allocator: std.mem.Allocator, instruction: Instruction) ![]const
     return header;
 }
 
-// used to switch
-const instruction_fields = enum(u8) { opcode, d, w, mod, reg, rm };
+const reg_arr = [8][2][]const u8{
+    .{ "al", "ax" },
+    .{ "cl", "cx" },
+    .{ "dl", "dx" },
+    .{ "bl", "bx" },
+    .{ "ah", "sp" },
+    .{ "ch", "bp" },
+    .{ "dh", "si" },
+    .{ "bh", "di" },
+};
 
 // holds the binary values of each field of an instruction
 const Instruction = struct {
