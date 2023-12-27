7da1430442b06b0d3d0e9490afa31be6c42648c8
Ben Muthalaly <benmuthalaly@gmail.com>
2023-12-27 01:31:57 -0600

    Add jump instructions, fix bugs

--- sim8086/src/encoding.zig
+++ sim8086/src/encoding.zig
@@ -9,7 +9,17 @@ const AssemblyIterator = struct {
         const index = self.index;
         const buf_end = if (index + 7 < self.buf.len) index + 7 else self.buf.len;
 
-        const parsed_instruction = parseInstruction(self.buf[index..buf_end]) catch return null;
+        const parsed_instruction = parseInstruction(self.buf[index..buf_end]) catch |err| {
+            switch (err) {
+                error.NoData => {
+                    print("no data!\n", .{});
+                },
+                error.FailedToParse => {
+                    print("failed to parse\n", .{});
+                },
+            }
+            return null;
+        };
         self.index += parsed_instruction.size;
         return parsed_instruction;
         // }
@@ -22,10 +32,11 @@ pub fn decode(allocator: std.mem.Allocator, data: []const u8) !std.ArrayList(u8)
     var iter = AssemblyIterator{ .buf = data };
 
     while (iter.next()) |instruction| {
-        const line = try binToAsm(allocator, instruction);
-        print("line: {s}\n", .{line});
+        const line = try instructionToAsm(allocator, instruction);
+        // print("line: {s}\n", .{line});
         try assembly.appendSlice(line);
     }
+    print("assembly:\n{s}\n", .{assembly.items});
     return assembly;
 }
 
@@ -33,7 +44,7 @@ pub fn parseInstruction(bytes: []const u8) !Instruction {
     if (bytes.len == 0) return error.NoData;
 
     var parsed_instruction: Instruction = undefined;
-    parsed_instruction.opcode = try parseOpcode(bytes[0]);
+    parsed_instruction.opcode = parseOpcode(bytes[0]);
     switch (parsed_instruction.opcode) {
         opcode_encoding.mov_normal, opcode_encoding.add_normal, opcode_encoding.sub_normal, opcode_encoding.cmp_normal => {
             parsed_instruction.d = (bytes[0] & 0b00000010) >> 1;
@@ -76,7 +87,7 @@ pub fn parseInstruction(bytes: []const u8) !Instruction {
                 parsed_instruction.data = hi | @as(u16, bytes[1]);
             }
         },
-        opcode_encoding.imm_to_from_with => {
+        opcode_encoding.imm_reg_or_mem => {
             parsed_instruction.s = (bytes[0] & 0b0000_0010) >> 1;
             parsed_instruction.w = (bytes[0] & 0b0000_0001) >> 0;
             parsed_instruction.mod = (bytes[1] & 0b1100_0000) >> 6;
@@ -85,18 +96,29 @@ pub fn parseInstruction(bytes: []const u8) !Instruction {
 
             var size: u8 = 2;
             switch (parsed_instruction.mod) {
-                0b00 => {},
+                0b00 => {
+                    // size += 1;
+                    if (parsed_instruction.rm == 0b110) {
+                        size += 2;
+                    }
+                },
                 0b01 => {
-                    parsed_instruction.disp = bytes[size];
+                    parsed_instruction.disp = bytes[2];
                     size += 1;
                 },
                 0b10 => {
-                    const hi = @shlExact(@as(u16, bytes[size + 1]), 8);
-                    parsed_instruction.disp = hi | @as(u16, bytes[size]);
+                    const hi = @shlExact(@as(u16, bytes[3]), 8);
+                    parsed_instruction.disp = hi | @as(u16, bytes[2]);
                     size += 2;
                 },
-                0b11 => {},
-                else => return error.FailedToParse,
+                0b11 => {
+                    // size += 1;
+                },
+                else => {
+                    print("Error: failed to parse mod field\n", .{});
+                    print("parsed instruction: {}\n", .{parsed_instruction});
+                    return error.FailedToParse;
+                },
             }
 
             if (parsed_instruction.s == 0 and parsed_instruction.w == 1) {
@@ -105,12 +127,9 @@ pub fn parseInstruction(bytes: []const u8) !Instruction {
                 size += 2;
             } else {
                 parsed_instruction.data = bytes[size];
+                size += 1;
             }
             parsed_instruction.size = size;
-            // switch (octal) {
-            //     0b000 => parsed_instruction.octal = 0b000,
-            //     0b101 => parsed_instruction.octal =
-            // }
         },
         opcode_encoding.add_imm_to_acc,
         opcode_encoding.sub_imm_from_acc,
@@ -126,14 +145,19 @@ pub fn parseInstruction(bytes: []const u8) !Instruction {
                 parsed_instruction.data = hi | @as(u16, bytes[1]);
             }
         },
+        // it's a jump
+        else => {
+            parsed_instruction.size = 2;
+            parsed_instruction.data = bytes[1];
+        },
     }
     return parsed_instruction;
 }
 
-fn parseOpcode(byte: u8) !opcode_encoding {
+fn parseOpcode(byte: u8) opcode_encoding {
     // add, sub, or cmp, look at second byte to know
     if (((byte & 0b1111_1100) ^ 0b1000_0000) == 0) {
-        return opcode_encoding.imm_to_from_with;
+        return opcode_encoding.imm_reg_or_mem;
     }
 
     if (((byte & 0b1111_0000) ^ 0b1011_0000) == 0) {
@@ -157,14 +181,19 @@ fn parseOpcode(byte: u8) !opcode_encoding {
         return opcode_encoding.sub_imm_from_acc;
     }
 
-    if (((byte & 0b1111_1100) ^ 0b0001_1100) == 0) {
+    if (((byte & 0b1111_1100) ^ 0b0011_1000) == 0) {
         return opcode_encoding.cmp_normal;
     }
     if (((byte & 0b1111_1110) ^ 0b0011_1100) == 0) {
         return opcode_encoding.cmp_imm_with_acc;
     }
 
-    return error.FailedToParse;
+    // parse jumps
+    return @enumFromInt(byte);
+
+    // print("Failed to parse opcode\n", .{});
+    // print("input byte: {b:0>8}\n", .{byte});
+    // return error.FailedToParse;
 }
 
 fn calcAddress(allocator: std.mem.Allocator, instruction: Instruction) ![]const u8 {
@@ -207,12 +236,13 @@ fn calcAddress(allocator: std.mem.Allocator, instruction: Instruction) ![]const 
 }
 
 /// returns a line of assembly translated from a parsed instruction
-fn binToAsm(allocator: std.mem.Allocator, instruction: Instruction) ![]const u8 {
+fn instructionToAsm(allocator: std.mem.Allocator, instruction: Instruction) ![]const u8 {
     var dst_str: []const u8 = undefined;
     var src_str: []const u8 = undefined;
 
     var formula: []const u8 = undefined;
-    print("opcode: {any}\n", .{instruction.opcode});
+    // print("opcode: {any}\n", .{instruction.opcode});
+    // print("instruction:\n{any}\n", .{instruction});
     switch (instruction.opcode) {
         opcode_encoding.mov_normal,
         opcode_encoding.add_normal,
@@ -243,15 +273,22 @@ fn binToAsm(allocator: std.mem.Allocator, instruction: Instruction) ![]const u8 
             dst_str = registers[instruction.reg][instruction.w];
         },
 
-        opcode_encoding.imm_to_from_with => {
-            dst_str = try calcAddress(allocator, instruction);
+        opcode_encoding.imm_reg_or_mem => {
+            if (instruction.mod == 0b11) {
+                dst_str = registers[instruction.rm][instruction.w];
+            } else {
+                dst_str = try calcAddress(allocator, instruction);
+            }
         },
 
         opcode_encoding.add_imm_to_acc,
         opcode_encoding.sub_imm_from_acc,
         opcode_encoding.cmp_imm_with_acc,
-        => {},
-        // else => return error.UnhandledOpcode,
+        => {
+            // print("ACC UNHANDLED\n", .{});
+            // return error.AccUnhandled;
+        },
+        else => {},
     }
 
     var opcode: []const u8 = undefined;
@@ -272,13 +309,16 @@ fn binToAsm(allocator: std.mem.Allocator, instruction: Instruction) ![]const u8 
         opcode_encoding.cmp_imm_with_acc,
         => opcode = "cmp",
 
-        opcode_encoding.imm_to_from_with => {
+        opcode_encoding.imm_reg_or_mem => {
             switch (instruction.opc_ext) {
                 0b000 => opcode = "add",
                 0b101 => opcode = "sub",
                 0b111 => opcode = "cmp",
                 else => return error.UnhandledOpcode,
             }
+        },
+        else => |opc| {
+            opcode = @tagName(opc);
         },
     }
 
@@ -314,12 +354,12 @@ fn binToAsm(allocator: std.mem.Allocator, instruction: Instruction) ![]const u8 
             instruction_parts = try allocator.alloc([]const u8, 6);
             instruction_parts[0] = opcode;
             instruction_parts[1] = " ";
-            instruction_parts[2] = "ax";
+            instruction_parts[2] = registers[0][instruction.w];
             instruction_parts[3] = ", ";
             instruction_parts[4] = try std.fmt.allocPrint(allocator, "{d}", .{instruction.data});
             instruction_parts[5] = "\n";
         },
-        opcode_encoding.imm_to_from_with => {
+        opcode_encoding.imm_reg_or_mem => {
             instruction_parts = try allocator.alloc([]const u8, 6);
             instruction_parts[0] = opcode;
             instruction_parts[1] = " ";
@@ -327,6 +367,13 @@ fn binToAsm(allocator: std.mem.Allocator, instruction: Instruction) ![]const u8 
             instruction_parts[3] = ", ";
             instruction_parts[4] = try std.fmt.allocPrint(allocator, "{d}", .{instruction.data});
             instruction_parts[5] = "\n";
+        },
+        else => {
+            instruction_parts = try allocator.alloc([]const u8, 4);
+            instruction_parts[0] = opcode;
+            instruction_parts[1] = " ";
+            instruction_parts[2] = try std.fmt.allocPrint(allocator, "{d}", .{instruction.data});
+            instruction_parts[3] = "\n";
         },
     }
 
@@ -396,25 +443,42 @@ const Instruction = struct {
     }
 };
 
-// TODO: expand to more opcodes
-// NOTE: should be a map?
 const opcode_encoding = enum(u8) {
-    imm_to_from_with = 0b100000,
+    imm_reg_or_mem = 0b100000,
 
     mov_normal = 0b100010,
     mov_imm_to_reg = 0b1011,
 
     add_normal = 0b000000,
-    // add_imm_to = 0b100000,
     add_imm_to_acc = 0b0000010,
 
     sub_normal = 0b001010,
-    // sub_imm_from = 0b100000,
     sub_imm_from_acc = 0b0010110,
 
     cmp_normal = 0b001110,
-    // cmp_imm_with = 0b100000,
     cmp_imm_with_acc = 0b0011110,
+
+    // jumps
+    je = 0b01110100,
+    jl = 0b01111100,
+    jle = 0b01111110,
+    jb = 0b01110010,
+    jbe = 0b01110110,
+    jp = 0b01111010,
+    jo = 0b01110000,
+    js = 0b01111000,
+    jne = 0b01110101,
+    jnl = 0b01111101,
+    jnle = 0b01111111,
+    jnb = 0b01110011,
+    jnbe = 0b01110111,
+    jnp = 0b01111011,
+    jno = 0b01110001,
+    jns = 0b01111001,
+    loop = 0b11100010,
+    loopz = 0b11100001,
+    loopnz = 0b11100000,
+    jcxz = 0b11100011,
 };
 
 const effective_address_calculation = [_][]const u8{
@@ -446,3 +510,25 @@ test "decode" {
     defer assembly.deinit();
     print("assembly: \n{s}\n", .{assembly.items});
 }
+// opcode_encoding.je => |opc| opcode = @tagName(opc),
+// opcode_encoding.jl => |opc| opcode = @tagName(opc),
+// opcode_encoding.jle => |opc| opcode = @tagName(opc),
+// opcode_encoding.jb => |opc| opcode = @tagName(opc),
+// opcode_encoding.jbe => |opc| opcode = @tagName(opc),
+// opcode_encoding.jp => |opc| opcode = @tagName(opc),
+// opcode_encoding.jo => |opc| opcode = @tagName(opc),
+// opcode_encoding.js => |opc| opcode = @tagName(opc),
+// opcode_encoding.jne => |opc| opcode = @tagName(opc),
+// opcode_encoding.jnl => |opc| opcode = @tagName(opc),
+// opcode_encoding.jnle => |opc| opcode = @tagName(opc),
+// opcode_encoding.jnb => |opc| opcode = @tagName(opc),
+// opcode_encoding.jnbe => |opc| opcode = @tagName(opc),
+// opcode_encoding.jnp => |opc| opcode = @tagName(opc),
+// opcode_encoding.jno => |opc| opcode = @tagName(opc),
+// opcode_encoding.jns => |opc| opcode = @tagName(opc),
+// opcode_encoding.loop => |opc| opcode = @tagName(opc),
+// opcode_encoding.loopz => |opc| opcode = @tagName(opc),
+// opcode_encoding.loopnz => |opc| opcode = @tagName(opc),
+// opcode_encoding.jcxz => |opc| opcode = @tagName(opc),
+
+// opcode_encoding.je, opcode_encoding.jl, opcode_encoding.jle, opcode_encoding.jb, opcode_encoding.jbe, opcode_encoding.jp, opcode_encoding.jo, opcode_encoding.js, opcode_encoding.jne, opcode_encoding.jnl, opcode_encoding.jnle, opcode_encoding.jnb, opcode_encoding.jnbe, opcode_encoding.jnp, opcode_encoding.jno, opcode_encoding.jns, opcode_encoding.loop, opcode_encoding.loopz, opcode_encoding.loopnz, opcode_encoding.jcxz => |opc| opcode = @tagName(opc),
